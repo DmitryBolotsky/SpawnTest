bd86ef5e1b36fe77829890434cd964288d161028
DmitryBolotsky <bolotskid@gmail.com>
2022-11-01 19:38:22 +0300

    Недостающее(оптимизация пулом)

---
+++ Assets/Scripts/PoolMono.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class PoolMono<T> where T : MonoBehaviour
+{
+    public T prefab { get; }
+    public bool autoExpand;
+    public Transform container { get; }
+
+    private List<T> pool;
+    
+    //конструктор
+    public PoolMono(T prefab, int count, Transform container)
+    {
+        this.prefab = prefab;
+        this.container = container;
+        this.CreatePool(count);
+    }
+
+    //создание пула
+    private void CreatePool(int count)
+    {
+        this.pool = new List<T>();
+        for (int i = 0; i < count; i++)
+        {
+            this.CreateObject();
+        }
+    }
+    //создание объектов пула из префаба по умолчанию они все не активны
+    private T CreateObject(bool isActiveByDefault = false)
+    {
+        var createdObject = Object.Instantiate(this.prefab, this.container.position,Quaternion.identity);
+        createdObject.gameObject.SetActive(isActiveByDefault);
+        this.pool.Add(createdObject);
+        return createdObject;
+    }
+
+    //проверка на свободный элесент если он есть то он возвращается в переменную element для повторного использования
+    public bool HasFreeElement(out T element)
+    {
+        foreach (var mono in pool)
+        {
+            if (!mono.gameObject.activeInHierarchy)
+            {
+                element = mono;
+                mono.gameObject.SetActive(true);
+                return true;
+            }
+        }
+
+        element = null;
+        return false;
+    }
+    //получение свободного элемента, если он не находится и пул авторасширяемый то создается новый элемент, причем сразу активный. В противном случае сообщение о том, что свободного элемента нет
+    public T GetFreeElement()
+    {
+        if (this.HasFreeElement(out var element))
+        {
+            return element;
+        }
+
+        if (this.autoExpand)
+        {
+            return this.CreateObject(true);
+        }
+
+        throw new Exception("No free elements in pool");
+    }
+}
